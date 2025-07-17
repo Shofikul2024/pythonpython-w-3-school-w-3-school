c2c7cf52ea27dd5041e10d3a8cdc19dd79786c07
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-17 23:51:47 +0600

    first commit

--- python-variable.py
+++ python-variable.py
@@ -16,3 +16,38 @@
 
 
 
+
+
+
+# type check of python
+ 
+
+# x=5
+# y='jhon'
+# print(type(x))
+# print(type(y))
+
+
+
+
+
+
+# myvar='jhon'
+# print(myvar)
+
+
+myvar = "John"
+print(myvar)
+
+
+my_var = "John"
+print(my_var)
+
+_my_var = "John"
+print(_my_var)
+myVar = "John"
+print(myVar)
+MYVAR = "John"
+print(MYVAR)
+myvar2 = "John"
+print(myvar2)
