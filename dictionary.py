683d0a3f7c38b40ba6ff0a3114c753a579f3d9fc
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-21 01:02:32 +0600

    again  added acces value

--- dictionary.py
+++ dictionary.py
@@ -23,17 +23,80 @@
 
 
 
-thisDict={
+# thisDict={
   
-  'brand':'Ford',
-  'model':'Mustang',
-  'year':'19644',
+#   'brand':'Ford',
+#   'model':'Mustang',
+#   'year':'19644',
   
   
-}
-print(thisDict['brand'])
+# }
+# print(thisDict['brand'])
 
 
 
 
 
+#duplicate  value  not allow
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "year": 2020
+# }
+# print(thisdict)
+
+
+
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# print(thisdict["year"])
+
+
+
+
+
+
+
+
+
+
+
+# type check
+
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# print(type(thisdict))
+
+
+
+
+
+
+# thisdict = dict(name = "John", age = 36, country = "Norway")
+# print(thisdict)
+
+
+a = {'name' : 'MD. Maksudur Rahman Khan', 'nickname' : 'Maateen', 'email' : 'maateen@outlook.com', 'phone' : '01711223344'}
+ 
+
+dictionaryAccess=a['name']
+
+print(dictionaryAccess)
+
+
+a['name']='shofikul islam'
+# print(dictionaryAccess)
+print(a)
+print(type(a))
