86f348e026b20af2e1ea15c651b8b6d1a88d96b1
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-20 23:01:37 +0600

    4 rd commit

--- python-variable.py
+++ python-variable.py
@@ -36,18 +36,87 @@
 # print(myvar)
 
 
-myvar = "John"
-print(myvar)
+# myvar = "John"
+# print(myvar)
 
 
-my_var = "John"
-print(my_var)
+# my_var = "John"
+# print(my_var)
 
-_my_var = "John"
-print(_my_var)
-myVar = "John"
-print(myVar)
-MYVAR = "John"
-print(MYVAR)
-myvar2 = "John"
-print(myvar2)
+# _my_var = "John"
+# print(_my_var)
+# myVar = "John"
+# print(myVar)
+# MYVAR = "John"
+# print(MYVAR)
+# myvar2 = "John"
+# print(myvar2)
+
+
+
+
+
+
+
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+
+
+
+
+# x,y,z='orange', 'bangla', 'cherry'
+# print(x)
+# print(y)
+# print(z)
+
+
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# x=y=z='orange'
+# print(x)
+# print(y)
+# print(z)
+
+
+
+
+
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+
+
+
+
+# fruits =['apple', 'banana', 'cherry']
+# x,y,z=fruits
+# print(x)
+# print(y)
+# print(z)
+
+
+
+
+
+
+fruits =['apple', 'banana', 'cherry']
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+
