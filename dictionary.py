a1585e361226355338b7172ed7627064b26a7eeb
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-20 23:37:36 +0600

    again  added  rang

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,19 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
 
-print(thisdict)
+# print(thisdict)
+
+
+
+thisDict={
+  
+  'brand':'Ford',
+  'model':'Mustang',
+  'year':'19644',
+  
+  
+}
+print(thisDict)
